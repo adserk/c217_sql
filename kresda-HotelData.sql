6fdd69ad2ec1a53e813ee07c50c8069301061287
adserk <48297079+adserk@users.noreply.github.com>
2022-04-28 22:05:12 +0100

    Commit Exercise HotelData

---
+++ kresda-HotelData.sql
@@ -0,0 +1,94 @@
+-- Part 3: Manage the Data
+USE HotelData;
+INSERT INTO Guest (FirstName, LastName, Address, City, State, Zip, Phone) 
+VALUES 
+	('Kresda', 'Rattanasudsai', '965 Surrey Street', 'Oswego', 'NY', '13126', '(834) 986-1390'),
+	('Mack', 'Simmer', '379 Old Shore Street','Council Bluffs', 'IA', '51501', '(291) 553-0508'),
+	('Bettyann', 'Seery', '750 Wintergreen Dr.', 'Wasilla',	'AK', '99654', '(478) 277-9632'),
+    ('Duane', 'Cullison', '9662 Foxrun Lane', 'Harlingen', 'TX', '78552', '(308) 494-0198'),
+    ('Karie', 'Yang', '9378 W. Augusta Ave.', 'West Deptford', 'NJ', '08096', '(214) 730-0298'),
+    ('Aurore', 'Lipton', '762 Wild Rose Street', 'Saginaw', 'MI', '48601', '(377) 507-0974'),
+    ('Zachery', 'Luechtefeld', '7 Poplar Dr.',	'Arvada', 'CO', '80003', '(814) 485-2615'),
+    ('Jeremiah', 'Pendergrass',	'70 Oakwood St.', 'Zion', 'IL',	'60099', '(279) 491-0960'),
+    ('Walter', 'Holaway', '7556 Arrowhead St.', 'Cumberland', 'RI', '02864', '(446) 396-6785'),
+    ('Wilfred', 'Vise', '77 West Surrey Street', 'Oswego', 'NY', '13126', '(834) 727-1001'),
+    ('Maritza', 'Tilton', '939 Linda Rd.', 'Burke', 'VA', '22015', '(446) 351-6860'),
+    ('Joleen', 'Tison', '87 Queen St.', 'Drexel Hill',	'PA', '19026', '(231) 893-2755');
+
+INSERT INTO RoomType (RoomType, StandardOccupancy, MaximumOccupancy)
+VALUES
+('Single', '2', '2'),
+('Double', '2', '4'),
+('Suite', '3', '8');
+
+INSERT INTO RoomPrice (BasePrice, ExtraPerson, Jacuzzi)
+VALUES
+	('149.99', '0', '25'),
+	('174.99', '10', '25'),
+    ('399.99', '20', '25');
+    
+INSERT INTO Amenities (AmenitiesId, Amenities)
+VALUES 
+	('1', 'Microwave'),
+    ('2', 'Refrigerator'),
+    ('3', 'Jacuzzi'),
+    ('4', 'Oven'),
+    ('5', 'Microwave, Jacuzzi'),
+    ('6', 'Microwave, Refrigerator'),
+    ('7', 'Microwave, Refrigerator, Jacuzzi'),
+    ('8', 'Microwave, Refrigerator, Oven')
+    ;
+INSERT INTO RoomNumber (RoomNumber, AmenitiesID, ADA)
+VALUES 
+	('201', '5', 'No'),
+    ('202', '2', 'Yes'),
+    ('203', '5', 'No'),
+    ('204', '2', 'Yes'),
+    ('205', '7', 'No'),
+    ('206', '6', 'Yes'),
+    ('207', '7', 'No'),
+    ('208', '6', 'Yes'),
+    ('301', '5', 'No'),
+    ('302', '2', 'Yes'),
+    ('303', '5', 'No'),
+    ('304', '2', 'Yes'),
+    ('305', '7', 'No'),
+    ('306', '6', 'Yes'),
+    ('307', '7', 'No'),
+    ('308', '6', 'Yes'),
+    ('401', '8', 'Yes'),
+    ('402', '8', 'Yes');
+    
+INSERT INTO Reservation (GuestId, RoomTypeId, Adults, Children, StartDate, EndDate, RoomPriceId, RoomNumberId)
+VALUES 
+('2', '1', '1', '0', '2023-02-02', '2023-02-04', '1', '16'), 
+('3', '2', '2', '1', '2023-02-05', '2023-02-10', '2', '3'),
+('4', '1', '2', '0', '2023-02-22', '2023-02-24','1','13'),
+('5', '2', '2', '2', '2023-03-06', '2023-03-07', '2', '1'),
+('1', '1', '1','1', '2023-03-17', '2023-03-20','1','15'),
+('6', '2', '3', '0', '2023-03-18', '2023-03-23', '2', '10'),
+('7', '2', '2', '2', '2023-03-29', '2023-03-31', '2', '2'),
+('8', '2', '2','0', '2023-03-31', '2023-04-05', '2', '12'),
+('9', '2', '1', '0', '2023-04-09', '2023-04-13', '2', '9'),
+('10', '1', '1', '1', '2023-04-23', '2023-04-24', '1', '7'),
+('11', '3', '2', '4', '2023-05-30', '2023-06-02', '3', '17'),
+('12', '1', '2', '0', '2023-06-10', '2023-06-14', '1', '6'),
+('12', '1', '1', '0', '2023-06-10', '2023-06-14', '1', '8'),
+('6', '2', '3', '0', '2023-06-17', '2023-06-18', '2', '8'),
+('1', '1', '2', '0', '2023-06-28', '2023-07-02', '1', '5'),
+('9', '2', '3', '1', '2023-07-13', '2023-07-14', '2', '4'),
+('10', '3', '4', '2', '2023-07-18', '2023-07-21', '3', '17'),
+('3', '2', '2', '1', '2023-07-28', '2023-07-29', '2', '11'),
+('3', '1', '1', '0', '2023-08-30', '2023-09-01', '1', '13' ),
+('2', '1', '2', '0', '2023-09-16', '2023-09-17', '1', '8' ),
+('5', '2', '2', '2', '2023-09-13', '2023-09-15', '2', '3'),
+('4', '3', '2', '2', '2023-11-22', '2023-11-25', '3', '17'),
+('2', '1', '2', '0', '2023-11-22', '2023-11-25', '1', '6'),
+('2', '2', '2', '2', '2023-11-22', '2023-11-25', '2', '9'), 
+('11', '2', '2', '0', '2023-12-24', '2023-12-28', '2', '10')
+;
+
+
+DELETE FROM Reservation WHERE GuestId = '8';
+DELETE FROM Guest WHERE GuestId = '8';
+
---
+++ kresda-HotelQueries.sql
@@ -0,0 +1,145 @@
+-- Part 4: Query the Database
+-- 1. Write a query that returns a list of reservations 
+-- that end in July 2023, including the name of the guest, the room number(s), and the reservation dates.
+
+USE HotelData;
+SELECT 
+r.ReservationId AS `Reservation ID`, 
+CONCAT(g.FirstName, ' ', g.LastName) `Name`, 
+rn.RoomNumber AS `Room Number(s)`, 
+r.StartDate `Check-In Date`, 
+r.EndDate `Check-Out Date`
+
+FROM Guest g
+JOIN Reservation r
+	ON g.GuestId = r.GuestId
+JOIN RoomNumber rn
+	ON r.RoomNumberId = rn.RoomNumberId
+WHERE r.EndDate < '2023-07-31'
+
+
+-- 2. Write a query that returns a list of all 
+-- reservations for rooms with a jacuzzi, displaying the guest's name, 
+-- the room number, and the dates of the reservation.
+USE HotelData;
+SELECT 
+r.ReservationId AS `Reservation ID`, 
+CONCAT(g.FirstName, ' ', g.LastName) `Name`, 
+a.Amenities AS Amenities,
+rn.RoomNumber AS `Room Number(s)`, 
+r.StartDate `Check-In Date`, 
+r.EndDate `Check-Out Date`
+
+FROM Guest g
+JOIN Reservation r
+	ON g.GuestId = r.GuestId
+JOIN RoomNumber rn
+	ON r.RoomNumberId = rn.RoomNumberId
+JOIN Amenities a
+	ON rn.AmenitiesId = a.AmenitiesId
+WHERE Amenities LIKE '%Jacuzzi'
+
+-- 3. Write a query that returns all the rooms reserved for a specific guest, 
+-- including the guest's name, the room(s) reserved, the starting 
+-- date of the reservation, and how many people were included in the reservation. 
+-- (Choose a guest's name from the existing data.)
+USE HotelData;
+SELECT 
+r.ReservationId AS `Reservation ID`, 
+CONCAT(g.FirstName, ' ', g.LastName) `Name`, 
+rn.RoomNumber AS `Room Number(s)`, 
+(r.Adults + r.Children) AS `Total Number of Guests`,
+r.StartDate `Check-In Date`, 
+r.EndDate `Check-Out Date`
+
+FROM Guest g
+JOIN Reservation r
+	ON g.GuestId = r.GuestId
+JOIN RoomNumber rn
+	ON r.RoomNumberId = rn.RoomNumberId
+JOIN Amenities a
+	ON rn.AmenitiesId = a.AmenitiesId
+WHERE g.FirstName = "Mack" AND g.LastName = "Simmer"
+-- JOIN RoomType rt
+-- 	ON r.RoomtypeId = rt.RoomTypeId 
+
+-- 4. Write a query that returns a list of rooms, reservation ID, and per-room cost for each reservation. 
+-- The results should include all rooms, whether or not there is a reservation associated with the room.
+-- Need to use subtable to call RoomPrice.Jacuzzi when the room has jacuzzi
+-- Need to use subtable to call RoomPrice.ExtraPerson when the room has extra person
+-- Need to query The TotalRoomPrice differently for every rows 
+-- since amenities & Extra Person Vs StandardOccupancy varies
+SELECT 
+CASE WHEN a.Amenities LIKE '%Jacuzzi' THEN '25' ELSE '0' END AS JacuzziPrice,
+CASE WHEN r.Adults > rt.StandardOccupancy THEN 10*(r.Adults-rt.StandardOccupancy) ELSE 0 END AS `ExtraPersonPrice`,
+rn.RoomNumber, 
+a.Amenities, a.AmenitiesId,
+rt.StandardOccupancy,
+CONCAT(g.FirstName, ' ', g.LastName) Name, 
+r.Adults, r.StartDate, 
+r.EndDate, DATEDIFF(r.EndDate, r.StartDate) AS days, 
+(rp.BasePrice ) * DATEDIFF(r.EndDate, r.StartDate) AS TotalRoomCost
+
+FROM Guest g
+JOIN Reservation r
+	ON g.GuestId = r.GuestId
+JOIN RoomPrice rp
+	ON r.RoomPriceId = rp.RoomPriceId
+JOIN RoomNumber rn
+	ON r.RoomNumberId = rn.RoomNumberId
+JOIN RoomType rt
+	ON r.RoomTypeId = rt.RoomTypeId
+JOIN Amenities a
+	ON a.AmenitiesId = rn.AmenitiesId
+-- WHERE a.Amenities LIKE '%Jacuzzi' OR  
+-- rt.StandardOccupancy < r.Adults
+ORDER BY StartDate;
+
+-- 5. Write a query that returns all the rooms accommodating at least 
+-- three guests and that are reserved on any date in April 2023.
+USE HotelData;
+SELECT 
+r.ReservationId AS `Reservation ID`, 
+CONCAT(g.FirstName, ' ', g.LastName) `Name`, 
+rn.RoomNumber AS `Room Number(s)`, 
+(r.Adults + r.Children) AS `Total Number of Guests`,
+r.StartDate `Check-In Date`, 
+r.EndDate `Check-Out Date`
+
+FROM Guest g
+JOIN Reservation r
+	ON g.GuestId = r.GuestId
+JOIN RoomNumber rn
+	ON r.RoomNumberId = rn.RoomNumberId
+JOIN Amenities a
+	ON rn.AmenitiesId = a.AmenitiesId
+WHERE (r.Adults + r.Children) >= 3 AND 
+(r.StartDate BETWEEN '2023-04-01' AND '2023-04-30') 
+-- In this data there's no reservation with more than 3 guests AND in April 2023
+
+-- 6. Write a query that returns a list of all guest names and the number of reservations 
+-- per guest, sorted starting with the guest with the most reservations and then by the guest's last name.
+USE HotelData;
+SELECT r.GuestId,
+g.FirstName, g.LastName,
+COUNT(r.GuestId) AS reservation_count
+
+FROM Guest g
+JOIN Reservation r
+	ON g.GuestId = r.GuestId
+JOIN RoomNumber rn
+	ON r.RoomNumberId = rn.RoomNumberId
+GROUP BY r.GuestId
+ORDER BY COUNT(r.GuestId) DESC, g.LastName;
+
+-- 7. Write a query that displays the name, address, and phone number of 
+-- a guest based on their phone number. (Choose a phone number from the existing data.)
+USE HotelData;
+SELECT 
+g.FirstName AS `First Name`, 
+g.LastName AS `Last Name`, 
+g.Address AS `Address`,
+g.Phone AS `Phone Number`
+
+FROM Guest g
+WHERE Phone LIKE '(214) 730-0298';
