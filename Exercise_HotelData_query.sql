4d33fa5d7111cb4e28b50d80c277e93cc9cf9a9e
adserk <48297079+adserk@users.noreply.github.com>
2022-04-28 01:16:37 +0100

    Commit exercise HotelData

--- Exercise_HotelData_query.sql
+++ Exercise_HotelData_query.sql
@@ -1,11 +1,16 @@
 USE HotelData;
-
-SELECT rn.RoomNumber, g.FirstName, r.ReservationId, r.RoomTypeId, DATEDIFF(r.EndDate, r.StartDate) AS days, (rp.BasePrice+rp.Jacuzzi) * DATEDIFF(r.EndDate, r.StartDate)
+-- Need to use subtable to call RoomPrice.Jacuzzi when the room has jacuzzi
+-- Need to use subtable to call RoomPrice.ExtraPerson when the room has extra person 
+SELECT rn.RoomNumber, g.FirstName, g.LastName, r.Adults, r.Children, r.StartDate, 
+r.EndDate, DATEDIFF(r.EndDate, r.StartDate) AS days, 
+(rp.BasePrice) * DATEDIFF(r.EndDate, r.StartDate) AS TotalRoomCost
 FROM Guest g
 JOIN Reservation r
-ON g.GuestId = r.GuestId
+	ON g.GuestId = r.GuestId
 JOIN RoomPrice rp
-ON r.RoomPriceId = rp.RoomPriceId
+	ON r.RoomPriceId = rp.RoomPriceId
 JOIN RoomNumber rn
-ON r.RoomNumberId = rn.RoomNumberId
+	ON r.RoomNumberId = rn.RoomNumberId
+JOIN RoomType rt
+	ON r.RoomTypeId = rt.RoomTypeId
 ORDER BY StartDate;
