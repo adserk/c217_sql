be31248382dd42f78090b106f4e65ab984998db8
adserk <48297079+adserk@users.noreply.github.com>
2022-04-29 01:19:47 +0100

    Commit Exercise HotelData

--- Untitled.sql
+++ Untitled.sql
@@ -1,19 +1,18 @@
 USE HotelData;
--- Need to use subtable to call RoomPrice.Jacuzzi when the room has jacuzzi
--- Need to use subtable to call RoomPrice.ExtraPerson when the room has extra person
--- Need to query The TotalRoomPrice differently for every rows 
--- since amenities & Extra Person Vs StandardOccupancy varies
 SELECT 
-CASE WHEN a.Amenities LIKE '%Jacuzzi' THEN '25' ELSE '0' END AS JacuzziPrice,
-CASE WHEN r.Adults > rt.StandardOccupancy THEN 10*(r.Adults-rt.StandardOccupancy) ELSE 0 END AS `ExtraPersonPrice`,
-rn.RoomNumber, 
-a.Amenities, a.AmenitiesId,
-rt.StandardOccupancy,
-CONCAT(g.FirstName, ' ', g.LastName) Name, 
-r.Adults, r.StartDate, 
-r.EndDate, DATEDIFF(r.EndDate, r.StartDate) AS days, 
-(rp.BasePrice ) * DATEDIFF(r.EndDate, r.StartDate) AS TotalRoomCost
-
+r.ReservationId AS `Reservation ID`,
+CONCAT(g.FirstName, ' ', g.LastName) `Guest Name`, 
+rn.RoomNumber AS `Room Number`, 
+a.Amenities, 
+r.StartDate, 
+r.EndDate, 
+DATEDIFF(r.EndDate, r.StartDate) AS `Days Reserved`, 
+rp.BasePrice,
+CASE WHEN a.Amenities LIKE '%Jacuzzi' THEN '25' ELSE '0' END AS `JacuzziPrice`,
+CASE WHEN r.Adults-rt.StandardOccupancy > 0 THEN r.Adults-rt.StandardOccupancy ELSE 0 END AS `Extra Person`,
+CASE WHEN r.Adults-rt.StandardOccupancy > 0 THEN 10*(r.Adults-rt.StandardOccupancy) ELSE 0 END AS `Extra Person Price`,
+-- Need to use extra parenthesise to call on 'CASE WHEN' columns
+(SELECT (`Extra Person Price`+`JacuzziPrice`+rp.BasePrice)*(`Days Reserved`)) AS `TotalRoomCost`
 FROM Guest g
 JOIN Reservation r
 	ON g.GuestId = r.GuestId
@@ -25,6 +24,4 @@ JOIN RoomType rt
 	ON r.RoomTypeId = rt.RoomTypeId
 JOIN Amenities a
 	ON a.AmenitiesId = rn.AmenitiesId
--- WHERE a.Amenities LIKE '%Jacuzzi' OR  
--- rt.StandardOccupancy < r.Adults
-ORDER BY StartDate;
+ORDER BY rn.RoomNumber;
---
+++ newtestquery.sql
@@ -0,0 +1,43 @@
+USE HotelData;
+-- SELECT 
+-- r.ReservationId AS `Reservation ID`,
+-- CONCAT(g.FirstName, ' ', g.LastName) `Guest Name`, 
+-- rn.RoomNumber AS `Room Number`, 
+-- a.Amenities, 
+-- r.StartDate, 
+-- r.EndDate, 
+-- DATEDIFF(r.EndDate, r.StartDate) AS `Days Reserved`, 
+-- rp.BasePrice,
+-- CASE WHEN a.Amenities LIKE '%Jacuzzi' THEN '25' ELSE '0' END AS `JacuzziPrice`,
+-- CASE WHEN r.Adults-rt.StandardOccupancy > 0 THEN r.Adults-rt.StandardOccupancy ELSE 0 END AS `Extra Person`,
+-- CASE WHEN r.Adults-rt.StandardOccupancy > 0 THEN 10*(r.Adults-rt.StandardOccupancy) ELSE 0 END AS `Extra Person Price`,
+-- -- Need to use extra parenthesise to call on 'CASE WHEN' columns
+-- (SELECT (`Extra Person Price`+`JacuzziPrice`+rp.BasePrice)*(`Days Reserved`)) AS `TotalRoomCost`
+SELECT 
+r.ReservationId AS `Reservation ID`,
+CONCAT(g.FirstName, ' ', g.LastName) `Guest Name`, 
+rn.RoomNumber AS `Room Number`, 
+a.Amenities, 
+r.StartDate, 
+r.EndDate, 
+DATEDIFF(r.EndDate, r.StartDate) AS `Days Reserved`, 
+rp.BasePrice,
+CASE WHEN a.Amenities LIKE '%Jacuzzi' THEN '25' ELSE '0' END AS `JacuzziPrice`,
+CASE WHEN r.Adults-rt.StandardOccupancy > 0 THEN r.Adults-rt.StandardOccupancy ELSE 0 END AS `Extra Person`,
+CASE WHEN r.Adults-rt.StandardOccupancy > 0 THEN 10*(r.Adults-rt.StandardOccupancy) ELSE 0 END AS `Extra Person Price`,
+-- Need to use extra parenthesise to call on 'CASE WHEN' columns
+(SELECT (`Extra Person Price`+`JacuzziPrice`+rp.BasePrice)*(`Days Reserved`)) AS `TotalRoomCost`
+FROM Guest g
+LEFT JOIN Reservation r
+	ON g.GuestId = r.GuestId
+RIGHT OUTER JOIN RoomNumber rn
+	ON r.RoomNumberId = rn.RoomNumberId
+RIGHT JOIN RoomPrice rp
+ON r.RoomPriceId = rp.RoomPriceId
+-- JOIN RoomNumber rn
+-- 	ON r.RoomNumberId = rn.RoomNumberId
+RIGHT JOIN RoomType rt
+ON r.RoomTypeId = rt.RoomTypeId
+RIGHT JOIN Amenities a
+ON a.AmenitiesId = rn.AmenitiesId
+ORDER BY rn.RoomNumber;
